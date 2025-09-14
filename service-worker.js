9d4ec561a274e74a4947200495555d2e29f5beba
emanueloliveira411-boop <emanueloliveira411@gmail.com>
2025-09-14 13:03:26 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => { console.log('Service Worker: Instalado'); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
